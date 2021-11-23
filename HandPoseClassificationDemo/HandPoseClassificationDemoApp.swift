05a043ea86d2de7d8a6cebb9f867a90ba482e03b
ysak-y <yoshiaki.0614@gmail.com>
2021-11-23 15:01:24 +0900

    Initial Commit

---
+++ HandPoseClassificationDemo/HandPoseClassificationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandPoseClassificationDemoApp.swift
+//  HandPoseClassificationDemo
+//
+//  Created by Yoshiaki Yamada on 2021/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HandPoseClassificationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
